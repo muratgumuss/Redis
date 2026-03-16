d6578910c7dfeb1ba620f282099bb1e9c37a8716
Murat Gümüş <muratgumus05@gmail.com>
2026-03-16 16:39:44 +0300

    StackExchange.Redis 2

---
+++ RedisExchangeApi.Web/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeApi.Web.Services;
+using StackExchange.Redis;
+
+namespace RedisExchangeApi.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        private readonly RedisService _redisService;
+
+        protected readonly IDatabase db;
+
+        public BaseController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDatabase(1);
+        }
+    }
+}
